5540680e067111ab2b9a2696fe40545561a8a410
Mohammad Rahhal <m.r992@hotmail.com>
2015-07-27 19:06:58 +0300

    + NodeKind

---
+++ OfflineWeb.Core/NodeKind.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OfflineWeb
+{
+	[Flags]
+	public enum NodeKind
+	{
+		None   = 0x0,
+		Link   = 0x1 << 0,
+		Script = 0x1 << 1,
+		Other  = 0x1 << 2,
+
+		All    = Link | Script | Other,
+	}
+
+	internal static class NodeKindExtensions
+	{
+		public static bool Has(this NodeKind @this, NodeKind nodeKind)
+		{
+			return (@this & nodeKind) == nodeKind;
+		}
+	}
+}
