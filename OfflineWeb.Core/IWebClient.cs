d7154c69a10cbe3e296a0ae32cb21ac8ed32f03c
Mohammad Rahhal <m.r992@hotmail.com>
2015-07-27 18:49:57 +0300

    + IWebClient

---
+++ OfflineWeb.Core/IWebClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace OfflineWeb
+{
+	public interface IWebClient
+	{
+		Task<string> DownloadStringAsync(string address);
+	}
+
+	public class WebClientAdapter : IWebClient
+	{
+		private WebClient _webClient = new WebClient();
+
+		public Task<string> DownloadStringAsync(string address)
+		{
+			return _webClient.DownloadStringTaskAsync(address);
+		}
+	}
+}
