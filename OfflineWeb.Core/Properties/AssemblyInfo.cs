1839ed43ff8bb0fb72e49cceda82212acf2386f7
Mohammad Rahhal <m.r992@hotmail.com>
2015-07-27 18:48:58 +0300

    Initial commit

---
+++ OfflineWeb.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OfflineWeb.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MRH")]
+[assembly: AssemblyProduct("OfflineWeb.Core")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ad227dc0-5496-476a-a07a-51920b48a00e")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
