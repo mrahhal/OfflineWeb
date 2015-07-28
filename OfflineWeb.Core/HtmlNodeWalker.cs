4c314d8e94c86030a7c147280f8688c977ff61d5
Mohammad Rahhal <m.r992@hotmail.com>
2015-07-28 13:01:34 +0300

    + HtmlNodeWalker

---
+++ OfflineWeb.Core/HtmlNodeWalker.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using HtmlAgilityPack;
+
+namespace OfflineWeb
+{
+	public class HtmlNodeWalker
+	{
+		private HtmlNode _root;
+		private HtmlNodeWalkerMode _mode;
+		private IEnumerator<HtmlNode> _nodes;
+		private Dictionary<string, NodeKind> _map = new Dictionary<string, NodeKind>
+		{
+			{ "link", NodeKind.Link },
+			{ "script", NodeKind.Script },
+		};
+
+		public HtmlNodeWalker(HtmlNode root, HtmlNodeWalkerMode mode = HtmlNodeWalkerMode.Descendants)
+		{
+			if (root == null)
+				throw new ArgumentNullException(nameof(root));
+
+			_root = root;
+			_mode = mode;
+			Initialize();
+		}
+
+		public NodeDescriptor Walk()
+		{
+			if (!_nodes.MoveNext())
+			{
+				return null;
+			}
+			var node = _nodes.Current;
+			return new NodeDescriptor(node, GetKindFromNodeName(node.Name));
+		}
+
+		private NodeKind GetKindFromNodeName(string name)
+		{
+			name = name.ToLowerInvariant();
+			var kind = default(NodeKind);
+			if (_map.TryGetValue(name, out kind))
+			{
+				return kind;
+			}
+			return NodeKind.Other;
+		}
+
+		private void Initialize()
+		{
+			switch (_mode)
+			{
+				case HtmlNodeWalkerMode.Descendants:
+					_nodes = _root.Descendants().GetEnumerator();
+					break;
+				case HtmlNodeWalkerMode.Children:
+					_nodes = _root.ChildNodes.Nodes().GetEnumerator();
+					break;
+			}
+		}
+	}
+
+	public enum HtmlNodeWalkerMode
+	{
+		Descendants,
+		Children,
+	}
+}
---
+++ OfflineWeb.Tests/HtmlNodeWalkerTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HtmlAgilityPack;
+using Xunit;
+
+namespace OfflineWeb.Tests
+{
+	public class HtmlNodeWalkerTests
+	{
+		[Fact]
+		public void Walk_WithEmptyNode_ReturnsNull()
+		{
+			// Arrange
+			var root = HtmlNode.CreateNode("<div></div>");
+			var walker = new HtmlNodeWalker(root);
+
+			// Act
+			var nd = walker.Walk();
+
+			// Assert
+			Assert.Null(nd);
+		}
+
+		[Fact]
+		public void Walk_KnowsLinkNodes()
+		{
+			// Arrange
+			var root = HtmlNode.CreateNode(@"<div><link rel=""stylesheet"">some</div>");
+			var walker = new HtmlNodeWalker(root);
+
+			// Act
+			var nd = walker.Walk();
+
+			// Assert
+			Assert.NotNull(nd);
+			Assert.Equal(NodeKind.Link, nd.Kind);
+		}
+
+		[Fact]
+		public void Walk_KnowsScriptNodes()
+		{
+			// Arrange
+			var root = HtmlNode.CreateNode(@"<div><script></script>some</div>");
+			var walker = new HtmlNodeWalker(root);
+
+			// Act
+			var nd = walker.Walk();
+
+			// Assert
+			Assert.NotNull(nd);
+			Assert.Equal(NodeKind.Script, nd.Kind);
+		}
+
+		[Fact]
+		public void Walk_KnowsOtherNodes()
+		{
+			// Arrange
+			var root = HtmlNode.CreateNode(@"<div><sometag></sometag>some</div>");
+			var walker = new HtmlNodeWalker(root);
+
+			// Act
+			var nd = walker.Walk();
+
+			// Assert
+			Assert.NotNull(nd);
+			Assert.Equal(NodeKind.Other, nd.Kind);
+		}
+	}
+}
