ecbd5ba4f2c1b922c9ccfc49ebb24dcafc6b249d
Mohammad Rahhal <m.r992@hotmail.com>
2015-07-28 10:52:44 +0300

    + OfflineWeb.Tests.csproj

---
+++ OfflineWeb.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OfflineWeb.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OfflineWeb.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("42184d75-20ac-4ce9-be34-15015837c29d")]
+
+[assembly: AssemblyVersion("1.0.0.*")]
