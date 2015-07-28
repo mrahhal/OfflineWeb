fdf5a8883b9870b09654cc35aa0085952cf00903
Mohammad Rahhal <m.r992@hotmail.com>
2015-07-28 10:59:21 +0300

    + IVisitor

---
+++ OfflineWeb.Core/IVisitor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace OfflineWeb
+{
+	public interface IVisitor
+	{
+		/// <summary>
+		/// Gets the nodes we're interested in visiting.
+		/// </summary>
+		NodeKind InterestingNodes { get; }
+
+		/// <summary>
+		/// Called to visit an interesting node.
+		/// </summary>
+		/// <param name="context">The visiting context.</param>
+		/// <param name="node">The node that we're visiting.</param>
+		/// <returns>The same node if no transformation is needed, a newly created one if transformation has been done, or null to remove the node.</returns>
+		Task<HtmlNode> VisitAsync(VisitingContext context, HtmlNode node);
+	}
+}
