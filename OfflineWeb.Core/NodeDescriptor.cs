38689cced6d04c6e266a2a903f387f5c5521022b
Mohammad Rahhal <m.r992@hotmail.com>
2015-07-28 12:54:16 +0300

    + NodeDescriptor

---
+++ OfflineWeb.Core/NodeDescriptor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HtmlAgilityPack;
+
+namespace OfflineWeb
+{
+	public class NodeDescriptor
+	{
+		public NodeDescriptor(HtmlNode node, NodeKind kind)
+		{
+			if (node == null)
+				throw new ArgumentNullException(nameof(node));
+
+			Node = node;
+			Kind = kind;
+		}
+
+		public HtmlNode Node { get; }
+
+		public NodeKind Kind { get; }
+	}
+}
