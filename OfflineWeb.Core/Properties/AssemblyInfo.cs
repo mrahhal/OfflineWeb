cd1c02666ab15724940718f59dc3702f40fe734f
Mohammad Rahhal <m.r992@hotmail.com>
2015-07-28 10:56:34 +0300

    + VisitingContext

--- OfflineWeb.Core/Properties/AssemblyInfo.cs
+++ OfflineWeb.Core/Properties/AssemblyInfo.cs
@@ -13,5 +13,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("ad227dc0-5496-476a-a07a-51920b48a00e")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
---
+++ OfflineWeb.Core/VisitingContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OfflineWeb
+{
+	public class VisitingContext
+	{
+		private string _rawAddress;
+		private Uri _address;
+
+		public string RawAddress
+		{
+			get { return _rawAddress; }
+			set
+			{
+				_rawAddress = value;
+				_address = new Uri(value, UriKind.Absolute);
+			}
+		}
+
+		public Uri Address
+		{
+			get { return _address; }
+		}
+
+		public IWebClient WebClient { get; set; }
+	}
+}
